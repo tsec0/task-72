5310b0aef3543d5bbc20b78e7bc22f520c7e6218
Tsvetomir Dikov <tsvetomir.dikov@gmail.com>
2022-12-26 16:18:27 +0200

    solve: Add a Tags component to render
    using react we create atagscomponent

--- src/App.js
+++ src/App.js
@@ -1,20 +1,22 @@
 import "./App.css";
+import {Tags} from "./Tags";
 
 function App() {
   return (
-    <div className="App">
-      <section class="hero">
-        <div class="hero-body">
-          <p class="title">A React Task</p>
-          <p class="subtitle">by Boom.dev</p>
-        </div>
-      </section>
-      <div class="container is-fullhd">
-        <div class="notification">
-          Edit the <code>./src</code> folder to add components.
-        </div>
-      </div>
-    </div>
+    // <div className="App">
+    //   <section class="hero">
+    //     <div class="hero-body">
+    //       <p class="title">A React Task</p>
+    //       <p class="subtitle">by Boom.dev</p>
+    //     </div>
+    //   </section>
+    //   <div class="container is-fullhd">
+    //     <div class="notification">
+    //       Edit the <code>./src</code> folder to add components.
+    //     </div>
+    //   </div>
+    // </div>
+    <Tags tags = {["boomdotdev", "task", "tags", "react"]}/>
   );
 }
 
